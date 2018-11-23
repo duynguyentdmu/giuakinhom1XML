c4d1cbb423b8205524282af5e93c868d2486b50b
Nguyen Minh Thien <nguyenminhthiensmart@gmail.com>
2018-11-23 10:41:22 +0700

    update Read/Write

--- nhom1DemoXML/ThongSinhVien.cs
+++ nhom1DemoXML/ThongSinhVien.cs
@@ -13,9 +13,11 @@ namespace nhom1DemoXML
 {
     public partial class ThongSinhVien : Form
     {
+        private DataSet dataSet;
         public ThongSinhVien()
         {
             InitializeComponent();
+            dataSet = new DataSet();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -31,36 +33,28 @@ namespace nhom1DemoXML
         private void btnLoad_Click(object sender, EventArgs e)
         {
             
-                try
-                {
-                    XmlReader xmlFile;
-         
-                    xmlFile = XmlReader.Create("../../test.xml", new XmlReaderSettings());
-                    DataSet dataSet = new DataSet();
-                    dataSet.ReadXml(xmlFile);
-                    gridSach.DataSource = dataSet.Tables[0];
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Lỗi xảy ra khi đọc file: " + ex.ToString());
-                }
-            
+            try
+            {
+                if (dataSet.Tables.Count > 0) return;
+                dataSet.ReadXml("../../test.xml", XmlReadMode.Auto);
+                gridSach.DataSource = dataSet.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi xảy ra khi đọc file: " + ex.ToString());
+            }
         }
 
         private void btnWrite_Click(object sender, EventArgs e)
         {
-            //try
-            //{
-            //    XmlWriter xmlFile;
-            //    xmlFile = XmlWriter.Create("E:/TAI LIEU DAI HOC/HOC KI 7/CONG NGHE XML VA UNG DUNG/BAI GIUA KI NHOM/PROJECT/nhom1DemoXML/test.xml", new XmlWriterSettings());
-            //    DataSet dataSet = new DataSet();
-            //    gridSach.DataSource = dataSet.Tables[0];
-            //    dataSet.WriteXml(xmlFile, XmlWriteMode.IgnoreSchema);
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show("Lỗi xảy ra khi ghi file: " + ex.ToString());
-            //}
+            try
+            {
+                dataSet.WriteXml("../../test.xml", XmlWriteMode.IgnoreSchema);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi xảy ra khi ghi file: " + ex.ToString());
+            }
         }
     }
 }
